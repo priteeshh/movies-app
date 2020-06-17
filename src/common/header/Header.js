472eee650c1ed0e3ad9048877cc377672c60ed18
priteeshh <priteeshh@gmail.com>
2020-06-18 01:03:37 +0530

    Header added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
 
 
-class Header extends Component{
-    render(){
-        return(
-            <div>
-                 <Button variant="contained" color="default">Login</Button>
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <header className="app-header">
+                    <img src={logo} className="app-logo " alt="logo"/>
+                    <div className="login-button">
+                        <Button variant="contained" color="default">Login</Button>
+                    </div>
+                </header>
             </div>
         );
     }
